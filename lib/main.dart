23810300dd20889072933843291846d28fd01576
Leah D'souza <68814280+leahdsouza@users.noreply.github.com>
2022-02-09 21:24:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:skilled_worker_app/pages/create_new_account.dart';
+import 'package:skilled_worker_app/pages/settings.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -7,6 +8,7 @@ void main() {
 
     routes:{
       '/': (context)=> CreateAccount(),
+      '/second': (context) => SettingPage(),
     },
   ));
 }
